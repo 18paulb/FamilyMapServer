fb3533822fbdb73d1e9b900fba31199993c85cb9
Brandon <bjpaul99@gmail.com>
2022-11-01 17:27:16 -0600

    Fixed bug in test

--- src/test/java/Tests/FindEventServiceTest.java
+++ src/test/java/Tests/FindEventServiceTest.java
@@ -47,21 +47,20 @@ public class FindEventServiceTest {
     //PositiveTest
     @Test
     public void findEventTest() {
-        //Register User and Make Person
-        Event event = new Event("1", "brandonpaul", "1234", (float) 13.45, (float) 456.24, "United States", "Denver", "Birth", 1999);
-
         FindEventResult findResult = null;
+        Event event = new Event("1", "brandonpaul", "1234", (float) 13.45, (float) 456.24, "United States", "Denver", "Marriage", 2024);
         try {
-            RegisterRequest request = new RegisterRequest("brandonpaul", "password", "bjpaul99@gmail.com", "Brandon", "Paul", "m", "1a2b");
+            RegisterRequest request = new RegisterRequest("brandonpaul", "password", "bjpaul99@gmail.com", "Brandon", "Paul", "m");
             RegisterResult result = RegisterService.register(request);
 
-            AuthToken token = new AuthToken(result.getAuthtoken(), result.getUsername());
+            event.setPersonID(result.getPersonID());
 
             Connection conn = db.openConnection();
             eventDao = new EventDao(conn);
             eventDao.insert(event);
             db.closeConnection(true);
 
+            AuthToken token = new AuthToken(result.getAuthtoken(), result.getUsername());
             //Find the person
             findResult = FindEventService.eventResponse(token.getAuthtoken(), event.getEventID());
 
@@ -69,18 +68,16 @@ public class FindEventServiceTest {
             e.printStackTrace();
         }
 
-        Event foundEvent = findResult.getEvent();
-
         assertNotNull(findResult);
-        assertEquals(foundEvent.getEventID(), event.getEventID());
-        assertEquals(foundEvent.getAssociatedUsername(), event.getAssociatedUsername());
-        assertEquals(foundEvent.getPersonID(), event.getPersonID());
-        assertEquals(foundEvent.getLatitude(), event.getLatitude());
-        assertEquals(foundEvent.getLongitude(), event.getLongitude());
-        assertEquals(foundEvent.getCountry(), event.getCountry());
-        assertEquals(foundEvent.getCity(), event.getCity());
-        assertEquals(foundEvent.getEventType(), event.getEventType());
-        assertEquals(foundEvent.getYear(), event.getYear());
+        assertEquals(findResult.getEventID(), event.getEventID());
+        assertEquals(findResult.getAssociatedUsername(), event.getAssociatedUsername());
+        assertEquals(findResult.getPersonID(), event.getPersonID());
+        assertEquals(findResult.getLatitude(), event.getLatitude());
+        assertEquals(findResult.getLongitude(), event.getLongitude());
+        assertEquals(findResult.getCountry(), event.getCountry());
+        assertEquals(findResult.getCity(), event.getCity());
+        assertEquals(findResult.getEventType(), event.getEventType());
+        assertEquals(findResult.getYear(), event.getYear());
 
     }
 
